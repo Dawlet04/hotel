2e9c94d2c78bfffc926203c61227390b04f88508
Dawlet04 <saymanovaguliy@gmail.com>
2026-02-10 14:30:42 +0500

    sedd

--- api/views.py
+++ api/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from rest_framework import serializers
 from rest_framework.generics import ListAPIView, CreateAPIView,  RetrieveUpdateDestroyAPIView
-
 from .serializers import RoomsSerializer
 from hotelsapp.models import Room, Booking, Hotel
+
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 
 class RoomApiView(ListAPIView):
@@ -21,3 +26,69 @@ class RoomRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomsSerializer
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Create(APIView):
+    def post(self, request, *args, **kwargs):
+        return
